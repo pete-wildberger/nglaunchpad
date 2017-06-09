5a55c6b6208e0feb27fe3c19fe704de73c8ea73c
Peter Wildberger <pete.wildberger@gmail.com>
2017-06-09 13:42:20 -0500

    cars

--- public/scripts/client.js
+++ public/scripts/client.js
@@ -1,29 +1,49 @@
 console.log('js');
-var myApp = angular.module( 'myApp', []);
+var myApp = angular.module('myApp', []);
 
 ///add controller to myApp
-myApp.controller('angularintro', function(){
-console.log('NG');
-var vm = this;
-vm.showFront = false;
+myApp.controller('angularintro', function() {
+  console.log('NG');
+  var vm = this;
+  vm.showFront = false;
+  vm.cars = [];
 
-  vm.testClick = function(){
+  vm.addCar = function() {
+    console.log('in addCar');
 
-  if(vm.userInput === '' || vm.userInput === undefined) {
-    vm.output = '';
+    var newCar = {
+      year: vm.yearIn,
+      make: vm.makeIn,
+      model: vm.modelIn,
+      cost: vm.costIn
+    };
+    vm.cars.push(newCar);
+    console.log(vm.cars);
 
-  } else {
-  vm.output = ('you typed:' +  vm.userInput);
-  console.log('you typed:', vm.userInput);
-}
-vm.userInput = '';
-};
-vm.toggleShow = function(){
-  vm.showFront = !vm.showFront;
-};
+  };
+
+
+
+
+//
+// vm.testClick = function() {
+//
+//   if (vm.userInput === '' || vm.userInput === undefined) {
+//     vm.output = '';
+//   } else {
+//     vm.output = ('you typed:' + vm.userInput);
+//     console.log('you typed:', vm.userInput);
+//   }
+//
+//   vm.userInput = '';
+// };
+
+//
+// vm.toggleShow = function() {
+//     vm.showFront = !vm.showFront;
+//   };
 
 });
-
 
 // after creating module and controller they must be connected to html.
 // do this with ng-app and ng-controller
